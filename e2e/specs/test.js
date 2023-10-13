895e0157b65c856742b08bff51c55ea9e0989479
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-10-13 23:58:27 +0100

    WebDriver IO Installation

---
+++ e2e/specs/test.js
@@ -0,0 +1,6 @@
+describe('my awesome website', () => {
+    it('should do some assertions', async () => {
+        await browser.url('https://webdriver.io')
+        await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO')
+    })
+})
