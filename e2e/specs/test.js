36f96f27ced989ad5bdc78f14e90979c4400dcf2
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-10-16 22:29:32 +0100

    forgot password test

--- e2e/specs/test.js
+++ e2e/specs/test.js
@@ -12,4 +12,20 @@ describe('Hudl E2E Test Coverage', () => {
         await continueButton.click()
         await invalidLogin.waitForDisplayed()
     })
+
+    it('Forgot password', async () => {
+        await browser.url('https://identity.hudl.com/login?state=hKFo2SBrS2lyVXIweHJlNmtRa1FVajZNWXY5NEMzTWo3ejVMdaFupWxvZ2luo3RpZNkgQ2x6cHdFenI2TE84ZS00N0hWc29tLUJnQVBaSzBvammjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5&protocol=oauth2&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.hudl.com%2Fapp%2Fusers%2Foidc%2Fcallback&scope=openid%20profile%20email&nonce=kPK6SiJGba418NtYzBVjNgz2a1d3cYVtsnygAREuJsY%3D&response_mode=form_post&screen_hint=')
+        await expect(browser).toHaveTitle('Log In')
+        const forgotPasswordLink = await $('#forgot-password')
+        const emailResetBox = await $('#email-reset')
+        const resetPasswordButton = await $('#btn-reset')
+        const forgotPasswordMessage =  await $("//p[contains(text(),'reset password link')]")
+        await forgotPasswordLink.click()
+        await expect(browser).toHaveTitle('Forgot Password')
+        await emailResetBox.addValue('test@test.com')
+        await resetPasswordButton.click()
+        await forgotPasswordMessage.waitForDisplayed()
+    })
+
+    
 })
