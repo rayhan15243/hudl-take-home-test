44522bcdffbbe5749f9300fed28702907f6c798d
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-10-16 22:44:46 +0100

    Before Hook

--- e2e/specs/test.js
+++ e2e/specs/test.js
@@ -1,8 +1,13 @@
 
 describe('Hudl Login Functionality Automated Coverage', () => {
-    it('Incorrect password test', async () => {
+
+     beforeEach(async () => {
+        // runs before each test in this block
         await browser.url('https://identity.hudl.com/login?state=hKFo2SBrS2lyVXIweHJlNmtRa1FVajZNWXY5NEMzTWo3ejVMdaFupWxvZ2luo3RpZNkgQ2x6cHdFenI2TE84ZS00N0hWc29tLUJnQVBaSzBvammjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5&protocol=oauth2&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.hudl.com%2Fapp%2Fusers%2Foidc%2Fcallback&scope=openid%20profile%20email&nonce=kPK6SiJGba418NtYzBVjNgz2a1d3cYVtsnygAREuJsY%3D&response_mode=form_post&screen_hint=')
         await expect(browser).toHaveTitle('Log In')
+      });
+
+    it('Incorrect password test', async () => {
         const email = await $('#email')
         const password = await $('#password')
         const continueButton = await $('#logIn')
@@ -14,8 +19,6 @@ describe('Hudl Login Functionality Automated Coverage', () => {
     })
 
     it('Forgot password test', async () => {
-        await browser.url('https://identity.hudl.com/login?state=hKFo2SBrS2lyVXIweHJlNmtRa1FVajZNWXY5NEMzTWo3ejVMdaFupWxvZ2luo3RpZNkgQ2x6cHdFenI2TE84ZS00N0hWc29tLUJnQVBaSzBvammjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5&protocol=oauth2&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.hudl.com%2Fapp%2Fusers%2Foidc%2Fcallback&scope=openid%20profile%20email&nonce=kPK6SiJGba418NtYzBVjNgz2a1d3cYVtsnygAREuJsY%3D&response_mode=form_post&screen_hint=')
-        await expect(browser).toHaveTitle('Log In')
         const forgotPasswordLink = await $('#forgot-password')
         const emailResetBox = await $('#email-reset')
         const resetPasswordButton = await $('#btn-reset')
@@ -28,8 +31,6 @@ describe('Hudl Login Functionality Automated Coverage', () => {
     })
 
     it('Create account test', async () => {
-        await browser.url('https://identity.hudl.com/login?state=hKFo2SBrS2lyVXIweHJlNmtRa1FVajZNWXY5NEMzTWo3ejVMdaFupWxvZ2luo3RpZNkgQ2x6cHdFenI2TE84ZS00N0hWc29tLUJnQVBaSzBvammjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5&protocol=oauth2&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.hudl.com%2Fapp%2Fusers%2Foidc%2Fcallback&scope=openid%20profile%20email&nonce=kPK6SiJGba418NtYzBVjNgz2a1d3cYVtsnygAREuJsY%3D&response_mode=form_post&screen_hint=')
-        await expect(browser).toHaveTitle('Log In')
         const createAccountLink = await $('#btn-show-signup')
         const createAccountButton = await $('#btn-signup')
         const createAccountErrorMessage =  await $("//p[contains(text(),'required fields')]")
