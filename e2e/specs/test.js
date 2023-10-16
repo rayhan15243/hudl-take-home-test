33b8420af65993e7f973c7d5d94d25e7eba10246
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-10-16 23:17:36 +0100

    Change base url

--- e2e/specs/test.js
+++ e2e/specs/test.js
@@ -3,7 +3,7 @@ describe('Hudl Login Functionality Automated Coverage', () => {
 
     beforeEach(async () => {
         // runs before each test in this block
-        await browser.url('https://identity.hudl.com/login?state=hKFo2SBrS2lyVXIweHJlNmtRa1FVajZNWXY5NEMzTWo3ejVMdaFupWxvZ2luo3RpZNkgQ2x6cHdFenI2TE84ZS00N0hWc29tLUJnQVBaSzBvammjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5&protocol=oauth2&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.hudl.com%2Fapp%2Fusers%2Foidc%2Fcallback&scope=openid%20profile%20email&nonce=kPK6SiJGba418NtYzBVjNgz2a1d3cYVtsnygAREuJsY%3D&response_mode=form_post&screen_hint=')
+        await browser.url('/login?state=hKFo2SBrS2lyVXIweHJlNmtRa1FVajZNWXY5NEMzTWo3ejVMdaFupWxvZ2luo3RpZNkgQ2x6cHdFenI2TE84ZS00N0hWc29tLUJnQVBaSzBvammjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5&protocol=oauth2&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.hudl.com%2Fapp%2Fusers%2Foidc%2Fcallback&scope=openid%20profile%20email&nonce=kPK6SiJGba418NtYzBVjNgz2a1d3cYVtsnygAREuJsY%3D&response_mode=form_post&screen_hint=')
         await expect(browser).toHaveTitle('Log In')
       });
 
@@ -11,7 +11,7 @@ describe('Hudl Login Functionality Automated Coverage', () => {
         const email = await $('#email')
         const password = await $('#password')
         const continueButton = await $('#logIn')
-        const invalidLogin = await $("//p[contains(text(),'recognize')]")
+        const invalidLogin = await $("//p[contains(text(),'recognize that email and/or password')]")
         await email.addValue('rayhan15423@gmail.com')
 		await password.addValue('test')
         await continueButton.click()
