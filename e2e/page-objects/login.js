740be582163e7547a0dc59edc7010515759d2d60
Rayhan Chowdhury <rayhan.chowdhury@version1.com>
2023-10-17 23:15:56 +0100

    Refactored Login Tests with Page Objects

--- e2e/page-objects/login.js
+++ e2e/page-objects/login.js
@@ -5,7 +5,23 @@ class Login {
     }
 
     get loginWithEmptyInputFieldsMessage() {
-      return $("//p[contains(text(),'required! fields')]");
+      return $("//p[contains(text(),'Please fill in all of the required fields')]");
+    }
+
+    get emailBox() {
+      return $("#email");
+    }
+
+    get passwordBox() {
+      return $("#password");
+    }
+
+    get invalidLoginMessage() {
+        return $("//p[contains(text(),'recognize that email and/or password')]");
+    }
+
+    get expiredPasswordMessage() {
+        return $("//p[contains(text(),'Password change required')]");
     }
   
     async clickContinueButton() {
@@ -14,6 +30,22 @@ class Login {
 
     async waitForEmptyInputFieldsErrorMessage() {
       await this.loginWithEmptyInputFieldsMessage.waitForDisplayed();
+    }
+
+    async enterEmailAddress(email) {
+      await this.emailBox.addValue(email);
+    }
+
+    async enterPassword(password) {
+      await this.passwordBox.addValue(password);
+    }
+
+    async waitForInvalidLoginMessageErrorMessage() {
+        await this.invalidLoginMessage.waitForDisplayed();
+    }
+
+    async waitForExpiredErrorMessage() {
+        await this.expiredPasswordMessage.waitForDisplayed();
     }
   
   }
--- e2e/specs/test.js
+++ e2e/specs/test.js
@@ -7,36 +7,28 @@ const ThirdPartyLogin  = require("../page-objects/third-party-login");
 describe('Hudl Login Functionality Test', () => {
 
     beforeEach(async () => {
-        // runs before each test in this describe block
+        // Runs before each test in this describe block
         await browser.url('/login?state=hKFo2SBrS2lyVXIweHJlNmtRa1FVajZNWXY5NEMzTWo3ejVMdaFupWxvZ2luo3RpZNkgQ2x6cHdFenI2TE84ZS00N0hWc29tLUJnQVBaSzBvammjY2lk2SBuMTNSZmtIektvemFOeFdDNWRaUW9iZVdHZjRXalNuNQ&client=n13RfkHzKozaNxWC5dZQobeWGf4WjSn5&protocol=oauth2&response_type=id_token&redirect_uri=https%3A%2F%2Fwww.hudl.com%2Fapp%2Fusers%2Foidc%2Fcallback&scope=openid%20profile%20email&nonce=kPK6SiJGba418NtYzBVjNgz2a1d3cYVtsnygAREuJsY%3D&response_mode=form_post&screen_hint=')
         await expect(browser).toHaveTitle('Log In')
     });
 
     it('Login with empty input fields test', async () => {
-        await Login.clickContinueButton();
-        await Login.waitForEmptyInputFieldsErrorMessage();
+        await Login.clickContinueButton()
+        await Login.waitForEmptyInputFieldsErrorMessage()
     })
 
     it('Incorrect password test', async () => {
-        const email = await $('#email')
-        const password = await $('#password')
-        const continueButton = await $('#logIn')
-        const invalidLogin = await $("//p[contains(text(),'recognize that email and/or password')]")
-        await email.addValue('rayhan15423@gmail.com')
-		await password.addValue('test')
-        await continueButton.click()
-        await invalidLogin.waitForDisplayed()
+        await Login.enterEmailAddress("rayhan15243@gmail.com")
+        await Login.enterPassword()
+        await Login.clickContinueButton()
+        await Login.waitForInvalidLoginMessageErrorMessage()
     })
 
     it('Expired password test', async () => {
-        const email = await $('#email')
-        const password = await $('#password')
-        const continueButton = await $('#logIn')
-        const expiredPasswordMessage = await $("//p[contains(text(),'Password change required')]")
-        await email.addValue('test@test.com')
-		await password.addValue('test')
-        await continueButton.click()
-        await expiredPasswordMessage.waitForDisplayed()
+        await Login.enterEmailAddress("test@test.com")
+        await Login.enterPassword("test")
+        await Login.clickContinueButton()
+        await Login.waitForExpiredErrorMessage()
     })
 
     it('Forgot password test', async () => {
@@ -94,12 +86,9 @@ describe('Hudl Login Functionality Test', () => {
     })
 
     it('Succesful login test', async () => {
-        const email = await $('#email')
-        const password = await $('#password')
-        const continueButton = await $('#logIn')
-        await email.addValue('rayhan15243@gmail.com')
-		await password.addValue(process.env.PASSWORD)
-        await continueButton.click()
+        await Login.enterEmailAddress("rayhan15243@gmail.com")
+        await Login.enterPassword(process.env.PASSWORD)
+        await Login.clickContinueButton()
         await expect(browser).toHaveTitle('Home - Hudl')
     })
     
